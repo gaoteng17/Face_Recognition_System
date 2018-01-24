1229fa57a320fb335f4d438e4e94bf4055b26c4c
ZXQLLJ <1512510237@qq.com>
2018-01-24 19:08:08 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,17 +1,26 @@
-import httplib
- 
-httpClient = None
- 
-try:
-    httpClient = httplib.HTTPConnection('127.0.0.1', 8080, timeout=30)
-    httpClient.request('GET', '/')
- 
-    response = httpClient.getresponse()
-    print response.status
-    print response.reason
-    print response.read()
-except Exception, e:
-    print e
-finally:
-    if httpClient:
-        httpClient.close()
+# -*- coding: UTF-8 -*-
+
+import requests
+import json
+
+
+Identity={
+    'school_id':'20164762',
+    'name':'jieli',
+    'class':'1603',
+    'options':'delete',
+    'parameters':''
+}
+
+pictures = {  
+    'img':open("/home/jieli/Pictures/2017-11-06 10-40-36 的屏幕截图.png",'rb')
+}  
+  
+
+print(Identity)
+json=json.dumps(Identity)
+print(json)
+r = requests.post('http://123.56.223.156:80/',json)
+#r = requests.post('http://123.56.223.156:8080/img',Identity,files=pictures)
+print(r)
+
